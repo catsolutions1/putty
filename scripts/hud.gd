70cd0c54eb12a6d46c2ac8782086146e0a92052d
catsolutions1 <143562269+catsolutions1@users.noreply.github.com>
2025-06-10 14:17:28 -0700

    grid based movement

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var label_scene = preload("res://scenes/ui/debug_label_input.tscn")
 @onready var action_list = $VBoxContainer
 
-var input_actions: Dictionary = {
+const input_actions: Dictionary = {
 	"move_up": "Move Up",
 	"move_left": "Move Left",
 	"move_down": "Move Down",
@@ -27,6 +27,8 @@ func _create_action_list() -> void:
 		
 		var events = InputMap.action_get_events(action)
 		if events.size() > 0:
+			label.set("theme_override_font_sizes/font_size", 8)
+			label.set("texture_filter", "nearest")
 			label.text = input_actions[action] + ": " + events[0].as_text()
 		else:
 			label.text = ""
--- scripts/input_settings.gd
+++ scripts/input_settings.gd
@@ -6,19 +6,17 @@ extends Control
 var is_remapping: bool = false
 var action_to_remap = null
 var remapping_button = null
-var is_key_allowed: bool = false
 
-var input_actions: Dictionary = {
+const input_actions: Dictionary = {
 	"move_up": "Move Up",
 	"move_left": "Move Left",
 	"move_down": "Move Down",
 	"move_right": "Move Right",
-	"shoot": "Shoot",
 	"delete_random_key": "Delete Key",
 	"spacebar": "Space"
 }
 
-var deletable_actions: Dictionary = {
+const deletable_actions: Dictionary = {
 	0: "move_up",
 	1: "move_left",
 	2: "move_down",
@@ -95,11 +93,11 @@ func _create_action_list() -> void:
 		button.pressed.connect(_on_input_button_pressed.bind(button, action))
 
 
-func _update_action_list(button, event) -> void:
+func _update_action_list(button: Button, event: InputEvent) -> void:
 	button.find_child("label_input").text = event.as_text().trim_suffix(" (Physical)")
 
 
-func _on_input_button_pressed(button, action) -> void:
+func _on_input_button_pressed(button: Button, action: String) -> void:
 	if !is_remapping:
 		is_remapping = true
 		action_to_remap = action
@@ -108,66 +106,73 @@ func _on_input_button_pressed(button, action) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if is_remapping:
-		if (event is InputEventKey || event is InputEventMouseButton && event.pressed):
-			if event is InputEventMouseButton && event.double_click:
-				event.double_click = false
-			
-			if event is InputEventKey:
-				is_key_allowed = false
-				for i in allowed_keys.size():
-					if event.as_text_physical_keycode() == allowed_keys[i]:
-						is_key_allowed = true
-				if !is_key_allowed:
-					return
-			
-			if InputMap.action_has_event("move_up", event) || InputMap.action_has_event("move_left", event) || InputMap.action_has_event("move_right", event) || InputMap.action_has_event("move_down", event):
-				return
-			
-			InputMap.action_erase_events(action_to_remap)
-			InputMap.action_add_event(action_to_remap, event)
-			_update_action_list(remapping_button, event)
-			
-			is_remapping = false
-			action_to_remap = null
-			remapping_button = null
-			
-			accept_event()
-	
-	elif event is InputEventKey:
-		backup_rebind("move_up", event)
-		backup_rebind("move_left", event)
-		backup_rebind("move_down", event)
-		backup_rebind("move_right", event)
+	if event is InputEventKey:
+		if is_allowed_key(event):
+			if is_remapping:
+				rebind(event)
+			else:
+				for i in deletable_actions.size():
+					backup_rebind(deletable_actions.get(i), event)
 
 
 func _select_key_to_delete() -> void:
-	var x = deletable_actions.values().pick_random()
-	delete_key(x)
+	var available_actions: Array = []
+	for i in deletable_actions.size():
+		if InputMap.action_get_events(deletable_actions.get(i)) != []:
+			available_actions.append(deletable_actions.get(i))
+	
+	var x = available_actions.pick_random()
+	if x != null:
+		delete_key(x)
+	else:
+		print("you lose")
 
 
-func delete_key(action) -> bool:
+func delete_key(action: String) -> void:
 	if InputMap.action_get_events(action):
 		for i in allowed_keys.size():
 			if allowed_keys[i] == InputMap.action_get_events(action)[0].as_text_physical_keycode():
 				allowed_keys.remove_at(i)
 				break
+		
 		InputMap.action_erase_event(action, InputMap.action_get_events(action)[0])
 		is_remapping = false
 		_on_input_button_pressed(action_list.get_children()[deletable_actions.find_key(action)], action)
-		return true
-	else:
-		return false
 
 
-func backup_rebind(action, event) -> void:
+func rebind(event: InputEvent) -> void:
+	for i in deletable_actions.size():
+		if InputMap.action_has_event(deletable_actions.get(i), event):
+			return
+	
+	InputMap.action_erase_events(action_to_remap)
+	InputMap.action_add_event(action_to_remap, event)
+	_update_action_list(remapping_button, event)
+	
+	is_remapping = false
+	action_to_remap = null
+	remapping_button = null
+	
+	accept_event()
+
+
+func backup_rebind(action: String, event: InputEvent) -> void:
 	var other_actions: PackedStringArray = []
 	for i in deletable_actions.size():
 		if action != deletable_actions.get(i):
 			other_actions.append(deletable_actions.get(i))
+	
 	if !InputMap.action_get_events(action):
-			if InputMap.action_has_event(other_actions[0], event) || InputMap.action_has_event(other_actions[1], event) || InputMap.action_has_event(other_actions[2], event):
+		for i in other_actions.size():
+			if InputMap.action_has_event(other_actions[i], event):
 				return
-			else:
-				InputMap.action_add_event(action, event)
-				_update_action_list(action_list.get_children()[int(deletable_actions.find_key(action))], event)
+		InputMap.action_add_event(action, event)
+		_update_action_list(action_list.get_children()[deletable_actions.find_key(action)], event)
+		accept_event()
+
+
+func is_allowed_key(event: InputEvent) -> bool:
+	for i in allowed_keys.size():
+		if event.as_text_physical_keycode() == allowed_keys[i]:
+			return true
+	return false
