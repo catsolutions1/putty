00abc984d3cbcc6b96567adc492ae463d2c2ff82
catsolutions1 <143562269+catsolutions1@users.noreply.github.com>
2025-08-04 19:19:46 -0700

    add buttons

--- scripts/level.gd
+++ scripts/level.gd
@@ -5,12 +5,18 @@ var goal_scene = preload("res://scenes/entities/goal.tscn")
 
 func _ready() -> void:
 	SignalBus.ENEMY_DEFEATED.connect(_check_enemy_list)
+	SignalBus.BUTTON_TOGGLED.connect(_toggle_tiles)
 
 
 func _check_enemy_list() -> void:
 	for body in $playable_space.get_overlapping_bodies():
-		if !body.is_in_group("player") && !body.is_in_group("wall"):
+		if !body.is_in_group("player") && !body.is_in_group("world"):
 			return
 	var goal = goal_scene.instantiate()
 	goal.global_position = goal_position
 	add_child(goal)
+
+
+func _toggle_tiles() -> void:
+	for body in get_tree().get_nodes_in_group("toggleable"):
+		body.enabled = !body.enabled
--- scripts/signal_bus.gd
+++ scripts/signal_bus.gd
@@ -3,3 +3,4 @@ extends Node
 signal DAMAGE_TAKEN
 signal GOAL_REACHED
 signal ENEMY_DEFEATED
+signal BUTTON_TOGGLED
